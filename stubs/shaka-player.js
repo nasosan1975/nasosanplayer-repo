9b44f9359bda3bba43a0a178e0e7bcc678d911d1
nasosan1975 <nasosansoft@gmail.com>
2026-05-26 14:04:55 +0200

    aggiornamento

---
+++ stubs/shaka-player.js
@@ -0,0 +1,6 @@
+// Stub for react-native-track-player web dependency — not needed on Android
+module.exports = {};
+module.exports.default = {};
+module.exports.polyfill = { installAll: () => {} };
+module.exports.Player = class {};
+module.exports.ui = {};
